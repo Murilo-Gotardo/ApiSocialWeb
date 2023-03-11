ae651c945088b9b4b4a04cdb314472f6ff11d680
Murilo Gotardo <murilogotardo08@gmail.com>
2023-03-10 22:29:56 -0300

    Mudaça entre o pg

--- Infrastructure/ConnectionContext.cs
+++ Infrastructure/ConnectionContext.cs
@@ -13,10 +13,10 @@ namespace apiSocialWeb.Infrastructure
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
             => optionsBuilder.UseNpgsql(
-                "Server=postgresql://postgres:uH7v6fEBv7S3rqZb54hm@containers-us-west-51.railway.app:7889/railway;" +
+                "Server=postgresql://postgres:Q3JY8PwEg6zqxMUque5M@containers-us-west-17.railway.app:6345/railway;" +
                 "Port=5432;Database=railway;" +
                 "User Id=postgres;" +
-                "Password=uH7v6fEBv7S3rqZb54hm;"
+                "Password=Q3JY8PwEg6zqxMUque5M;"
                 ); 
     }
 }
