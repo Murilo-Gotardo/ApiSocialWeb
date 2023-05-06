b9b62a272d2a1f2873338908368e62b0f033bdb0
Murilo Gotardo <murilogotardo08@gmail.com>
2023-05-06 19:17:56 -0300

    ConnectionContext fixed

--- Infrastructure/ConnectionContext.cs
+++ Infrastructure/ConnectionContext.cs
@@ -20,7 +20,7 @@ namespace apiSocialWeb.Infrastructure
         //private readonly string connectionString = Environment.GetEnvironmentVariable("DATABASE_CONECTION_STRING_DEVELOPMENT").Replace(" ", "");
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-            => optionsBuilder.UseNpgsql($"{ConnectionString}");
+            => optionsBuilder.UseNpgsql($"{ConnectionString.Replace(" ", "")}");
 
         //"Server=localhost;Port=5432;Database=railway;Username=postgres;Password=1234;"
 
